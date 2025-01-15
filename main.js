2263bdecfb5ed95aa355630c2227e9f99fb2f9a6
GrandeGH <georgesgrande96@gmail.com>
2025-01-15 11:09:47 +0100

    1er commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// nom des patients
+
+class Patients {
+    constructor(nom, maladie, argent, poche, état_de_santé){
+        this.nom = nom
+        this.maladie = maladie
+        this.argent = argent 
+        this.poche = poche,
+        this.état_de_santé = état_de_santé
+    }
+}
+
+let Marcus = new Patients ("Marcus", "mal indenté", 100, "vide", "malade")
+let Optimus = new Patients ("Optimus", "unsave", 200, "vide", "malade")
+let Songoku = new Patients ("Son Goku", "404", 80, "vide", "malade")
+let DarthVader = new Patients ("Darth Vader", "azmatique", 110, "vide", "malade")
+let Semicolon = new Patients ("Semicolon", "syntaxError", 60, "vide", "malade")
+
+
+
